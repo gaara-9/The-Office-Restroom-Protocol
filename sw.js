2ecb56cc352ed1dc10230cf3f7c8b876a2f971a7
gaara-9 <66076702+gaara-9@users.noreply.github.com>
2025-12-05 22:02:36 +0530

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
-const CACHE_NAME = 'urinal-game-v1';
+const CACHE_NAME = 'urinal-protocol-v1';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdn.tailwindcss.com'
 ];
 
+// Install Event
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
+// Fetch Event
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
